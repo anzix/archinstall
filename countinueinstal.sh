d8648f5f300d06a722124d3fe96738da6e08fadb
AnZiePow <69862724+AnZiePow@users.noreply.github.com>
2021-08-11 02:20:35 +0500

    Update countinueinstal.sh

--- countinueinstal.sh
+++ countinueinstal.sh
@@ -1,7 +1,7 @@
 #Установка Yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+makepkg -si --noconfirm
 
 #Создания английских папок
 LC_ALL=C xdg-user-dirs-update --force
@@ -9,9 +9,12 @@ LC_ALL=C xdg-user-dirs-update --force
 #Установка i3
 yay -S --noconfirm i3-gaps rofi xorg xorg-xinit xorg-xrandr dunst i3status picom-git firefox nerd-fonts-ubuntu-mono ly-git
 
+#Установка видео дров для Vmware
+yay-S --noconfirm open-vm-tools xf86-video-vmware xf86-input-vmmouse xf86-video-vesa
+systemctl enable vmtoolsd
+
 systemctl enable ly.service
 
-echo reboot
 
 #sudo pacman -Syu
 #sudo pacman -S wget --noconfirm
