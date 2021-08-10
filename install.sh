7215347e4dff054c3182a149653714e02495e05e
AnZiePow <69862724+AnZiePow@users.noreply.github.com>
2021-08-10 12:47:22 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,7 +31,12 @@ echo 'LANG="ru_RU.UTF-8"' > /etc/locale.conf
 echo 'KEYMAP=ru' >> /etc/vconsole.conf
 echo 'FONT=cyr-sun16' >> /etc/vconsole.conf
 
-sh -c "sed -i '/\[multilib\]/,/Include/s/^[ ]*#//' /etc/pacman.conf"
+
+echo '[multilib]' >> /etc/pacman.conf
+echo 'Include = /etc/pacman.d/mirrorlist' >> /etc/pacman.conf
+
+#Если первая не сработает то включу вторую
+#sh -c "sed -i '/\[multilib\]/,/Include/s/^[ ]*#//' /etc/pacman.conf"
 
 
 #Надо проверить строчку hooks
@@ -48,3 +53,20 @@ sh -c "sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers"
 
 pacman -Syu
 pacman -S --noconfirm xorg-xinit xorg-server xorg-xrandr xdg-utils xdg-user-dirs links wget alacritty ranger pcmanfm-gtk3 gvfs file-roller unzip unrar pulseaudio alsa alsa-utils pulseaudio-alsa intel-ucode dhcpcd pavucontrol
+
+
+mkdir /boot/efi
+mount /dev/sda1 /boot/efi
+grub-install --efi-directory=/boot/efi
+grub-mkconfig -o /boot/grub/grub.cfg
+
+
+systemctl enable dhcocd.service
+
+exit
+umount -R /mnt
+reboot
+
+#sudo pacman -Syu
+#sudo pacman -S wget --noconfirm
+#wget git.io/yay-install.sh && sh yay-install.sh --noconfirm
