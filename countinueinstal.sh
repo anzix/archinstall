2b01c1797347e99ad23418139a3ded0208817600
AnZiePow <69862724+AnZiePow@users.noreply.github.com>
2021-08-10 14:25:28 +0500

    Create countinueinstal.sh

---
+++ countinueinstal.sh
@@ -0,0 +1,17 @@
+#Установка Yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+#Создания английских папок
+LC_ALL=C xdg-user-dirs-update --force
+
+#Установка i3
+yay -S --noconfirm i3-gaps rofi xorg xorg-xinit xorg-xrandr dunst i3status picom-git firefox nerd-fonts-ubuntu-mono ly-git
+
+systemctl enable ly.service
+
+
+#sudo pacman -Syu
+#sudo pacman -S wget --noconfirm
+#wget git.io/yay-install.sh && sh yay-install.sh --noconfirm
