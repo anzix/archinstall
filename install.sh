840cae41de7887349b7fe5c84152fed57f079c28
anzix <69862724+anzix@users.noreply.github.com>
2021-08-11 21:03:36 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 
 
 sgdisk --zap-all /dev/sda  # Delete tables
-printf "n\n1\n\n+100M\nef00\nn\n2\n\n\n+300m\nn\n\n3\n\n\n\nw\ny\n" | gdisk /dev/sda
+printf "n\n1\n\n+100M\nef00\nn\n2\n\n\n+300m\nef02\nn\n\n3\n\n\n\nw\ny\n" | gdisk /dev/sda
 
 mkfs.fat -F32 /dev/sda1
 mkfs.ext4 /dev/sda2
