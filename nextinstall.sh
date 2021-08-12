8092652352857e7c4aef70c02121f22d7ad1b048
anzix <69862724+anzix@users.noreply.github.com>
2021-08-13 01:50:43 +0500

    Update nextinstall.sh

--- nextinstall.sh
+++ nextinstall.sh
@@ -7,7 +7,7 @@ makepkg -si --noconfirm
 LC_ALL=C xdg-user-dirs-update --force
 
 #Установка i3
-yay -S --noconfirm i3-gaps rofi xorg xorg-xinit xorg-xrandr dunst i3status picom-git firefox nerd-fonts-ubuntu-mono ly-git open-vm-tools xf86-video-vmware xf86-input-vmmouse xf86-video-vesa
+yay -S --noconfirm i3-gaps rofi xorg xorg-xinit xorg-xrandr dunst i3status picom-git ttf-awesome ttf-font-awesome ttf-opensans ttf-kochi-substitute otf-ipafont noto-fonts ttf-droid ttf-liberation ttf-dejavu ttf-ubuntu-font-family firefox nerd-fonts-ubuntu-mono ly-git open-vm-tools xf86-video-vmware xf86-input-vmmouse xf86-video-vesa
 
 systemctl enable vmtoolsd
 
