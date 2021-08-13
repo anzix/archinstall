a605b55e657941600ca9db8ce977fbb72c0bf8fc
anzix <69862724+anzix@users.noreply.github.com>
2021-08-13 19:04:50 +0500

    Update nextinstall.sh

--- nextinstall.sh
+++ nextinstall.sh
@@ -7,10 +7,13 @@ makepkg -si --noconfirm
 LC_ALL=C xdg-user-dirs-update --force
 
 #Установка i3
-yay -S --noconfirm i3-gaps rofi xorg xorg-xinit xorg-xrandr dunst i3status picom-git timeshift timeshift-autosnap lxappearance clipit firefox polkit-gnome man-pages-ru kotatogram-desktop-bin bleachbit ttf-awesome ttf-font-awesome ttf-opensans ttf-kochi-substitute otf-ipafont noto-fonts ttf-droid ttf-liberation ttf-dejavu ttf-ubuntu-font-family nerd-fonts-ubuntu-mono powerline powerline-fonts ly-git open-vm-tools xf86-video-vmware xf86-input-vmmouse xf86-video-vesa
+yay -S --noconfirm i3-gaps rofi xorg xorg-xinit xorg-xrandr dunst i3status picom-git autotiling timeshift timeshift-autosnap lxappearance clipit firefox polkit-gnome man-pages-ru kotatogram-desktop-bin bleachbit ttf-awesome ttf-font-awesome ttf-opensans ttf-kochi-substitute otf-ipafont noto-fonts ttf-droid ttf-liberation ttf-dejavu ttf-ubuntu-font-family nerd-fonts-ubuntu-mono powerline powerline-fonts ly-git open-vm-tools xf86-video-vmware xf86-input-vmmouse xf86-video-vesa
+
+#Русская раскладка 
+localectl set-x11-keymap --no-convert us,ru pc105 "" grp:alt_shift_toggle
 
 systemctl enable vmtoolsd
-
+systemctl start vmtoolsd
 systemctl enable ly.service
 
 
