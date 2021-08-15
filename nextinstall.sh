262f278edae982749acce73873ace47bfe42867f
anzix <69862724+anzix@users.noreply.github.com>
2021-08-16 00:33:18 +0500

    Update nextinstall.sh

--- nextinstall.sh
+++ nextinstall.sh
@@ -5,6 +5,8 @@ makepkg -si --noconfirm
 
 #Создания английских папок
 LC_ALL=C xdg-user-dirs-update --force
+#Удаляем рус папки
+rmdir Видео Документы Загрузки Изображения Музыка Общедоступные Рабочий\ стол/ Шаблоны
 
 #Установка i3
 yay -S --noconfirm i3-gaps rofi xorg xorg-xinit xorg-xrandr dunst i3status picom autotiling timeshift timeshift-autosnap lxappearance clipit firefox polkit-gnome man-pages-ru kotatogram-desktop-bin bleachbit ttf-awesome ttf-font-awesome ttf-opensans ttf-kochi-substitute otf-ipafont noto-fonts ttf-droid ttf-liberation ttf-dejavu ttf-ubuntu-font-family nerd-fonts-ubuntu-mono powerline powerline-fonts ly-git open-vm-tools xf86-video-vmware xf86-input-vmmouse xf86-video-vesa
