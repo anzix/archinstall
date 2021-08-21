95ad0cd8907e9ff6e3a208f8dfcff01ffbe1d0e1
anzix <69862724+anzix@users.noreply.github.com>
2021-08-21 22:48:09 +0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -35,27 +35,23 @@ mkdir /mnt/{boot,home}
 mount -o noatime,compress=zstd:2,space_cache=v2,discard=async,subvol=@home /dev/sda3 /mnt/home
 mount /dev/sda2 /mnt/boot
 
-loadkeys ru
-setfont cyr-sun16
-
 #Правка конфига pacman
-
-sed -i "/#Color/a ILoveCandy" /etc/pacman.conf  # Making pacman prettier
-sed -i "s/#Color/Color/g" /etc/pacman.conf  # Add color to pacman
-sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 10/g" /etc/pacman.conf  # Parallel downloads
+sed -i "/#Color/a ILoveCandy" /etc/pacman.conf  # Делаем pacman красивее
+sed -i "s/#Color/Color/g" /etc/pacman.conf  # Добавляем цвета в pacman
+sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 10/g" /etc/pacman.conf  # Увеличение паралельных загрузок 
 sed -i "94,95s/^#//" /etc/pacman.conf # Раскоментирование строчки (В данном случае они именуются цифрами) multilib для запуска 32bit приложений
 
-#
 #sed -i "s/#[multilib]/[multilib]/g; s/#Include/Include/g" /etc/pacman.conf
 #sed -i "/[multilib\]/,/Include/s/^[ ]#//" /etc/pacman.conf
 
+# Оптимизация зеркал с помощью Reflector
 pacman -Sy --noconfirm rsync
 reflector --verbose -c ru,by,ua,de,pl -p https,http -l 12 --sort rate --save /etc/pacman.d/mirrorlist
 
 #Обновление пакетов
 pacman -Syy
 
-#Установка пакетов
+#Установка базовых пакетов
 pacstrap /mnt base base-devel linux-firmware linux-zen linux-zen-headers btrfs-progs grub efibootmgr zsh git nano vim
 
 #Созлание genfstab
@@ -69,7 +65,6 @@ pacman-key --init
 pacman-key --populate archlinux
 
 #Локализация на Русский
-#sed -i "/en_US.UTF-8/s/^#//g" /etc/locale.gen
 sed -i "/^#\en_SG ISO/{N;s/\n#/\n/}" /etc/locale.gen
 
 sed -i "/ru_RU.UTF-8/s/^#//g" /etc/locale.gen
@@ -95,17 +90,8 @@ echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
 echo "127.0.1.1 anzix.localdomain anzix" >> /etc/hosts
 
-
-#sed -i "/177;403s/^#//" /etc/locale.gen
-#sed -i "s/#en_US/en_US/g; s/#ru_RU/ru_RU/g" /etc/locale.gen
-#echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-#echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen 
-
-
 #Добавление в mkinitcpio модуль btrfs и правка hooks
-#echo -e "MODULES=(btrfs)\nHOOKS=(keymap)\"" > /etc/mkinitcpio.conf
 sed -i "s/^HOOKS.*/HOOKS=(base udev autodetect modconf block filesystems keyboard keymap)/g" /etc/mkinitcpio.conf
-#sed -i "s/^HOOKS.*/HOOKS=(keymap)/" /etc/mkinitcpio.conf
 sed -i 's/^MODULES.*/MODULES=(btrfs)/' /etc/mkinitcpio.conf
 #Создание образа ранней загрузки
 mkinitcpio -P
@@ -122,28 +108,27 @@ echo "anzix:anz" | chpasswd
 #Удалить права пароля Sudo
 sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
 
-sed -i "/#Color/a ILoveCandy" /etc/pacman.conf  # Making pacman prettier
-sed -i "s/#Color/Color/g" /etc/pacman.conf  # Add color to pacman
-sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 7/g" /etc/pacman.conf  # Parallel downloads
+# Правка конфига pacman
+sed -i "/#Color/a ILoveCandy" /etc/pacman.conf  # Делаем pacman красивее
+sed -i "s/#Color/Color/g" /etc/pacman.conf  # Добавляем цвета в pacman
+sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 7/g" /etc/pacman.conf  # Увеличение паралельных загрузок 
 sed -i "94,95s/^#//" /etc/pacman.conf # Раскоментирование строчки (В данном случае они именуются цифрами) multilib для запуска 32bit приложений
 
-
-#pacman-key --init
-
-#pacman-key --populate archlinux
-
+# Обноваление и установка необходимых пакетов (для установки i3 окружения)
 pacman -Syu
 pacman -S --noconfirm xorg-xinit xorg-server xorg-xrandr xdg-utils xdg-user-dirs links wget alacritty ranger pcmanfm-gtk3 gvfs file-roller unzip unrar pulseaudio alsa alsa-utils pulseaudio-alsa intel-ucode dhcpcd pavucontrol
 
-
+# Установка Grub
 mkdir /boot/efi
 mount /dev/sda1 /boot/efi
 grub-install --efi-directory=/boot/efi
 grub-mkconfig -o /boot/grub/grub.cfg
 
-
+# Включение dhcpcd
 systemctl enable dhcpcd.service
 
 EOF
-echo umount -R /mnt
+
+# Готово!
+umount -R /mnt
 
