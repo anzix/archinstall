9124e70bacc2a5936913950a70dd41a34d88f704
Михаил <givefly3@gmail.com>
2021-08-28 15:03:52 +0000

    Update nextinstall.sh

--- nextinstall.sh
+++ nextinstall.sh
@@ -5,11 +5,11 @@ makepkg -si --noconfirm
 
 #Создания английских папок
 LC_ALL=C xdg-user-dirs-update --force
-#Удаляем рус папки
+#Удаляем рус папки (Не удаляются, надо как-то это решить)_
 rmdir Видео Документы Загрузки Изображения Музыка Общедоступные Рабочий\ стол/ Шаблоны
 
-#Установка i3
-yay -S --noconfirm i3-gaps rofi xorg xorg-xinit xorg-xrandr dunst i3status picom autotiling timeshift timeshift-autosnap lxappearance clipit firefox polkit-gnome man-pages-ru kotatogram-desktop-bin bleachbit ttf-awesome ttf-font-awesome ttf-opensans ttf-kochi-substitute otf-ipafont noto-fonts ttf-droid ttf-liberation ttf-dejavu ttf-ubuntu-font-family nerd-fonts-ubuntu-mono powerline powerline-fonts ly-git open-vm-tools xf86-video-vmware xf86-input-vmmouse xf86-video-vesa
+#Установка i3 (Не устанваливаются aur пакеты, надо тоже что-то с этим сделать)
+yay -S --noconfirm i3-gaps rofi xorg xorg-xinit xorg-xrandr dunst i3status picom autotiling timeshift timeshift-autosnap lxappearance clipit firefox polkit-gnome man-pages-ru kotatogram-desktop-bin bleachbit ttf-font-awesome ttf-opensans ttf-kochi-substitute otf-ipafont noto-fonts ttf-droid ttf-liberation ttf-dejavu ttf-ubuntu-font-family nerd-fonts-ubuntu-mono powerline powerline-fonts ly-git open-vm-tools xf86-video-vmware xf86-input-vmmouse xf86-video-vesa
 
 #Русская раскладка 
 localectl set-x11-keymap --no-convert us,ru pc105 "" grp:alt_shift_toggle
