0de4b35b6a39e29fc4ea3d215abc2710670a8672
Михаил <givefly3@gmail.com>
2021-08-29 21:26:01 +0000

    Update nextinstall.sh

--- nextinstall.sh
+++ nextinstall.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 # Установка Yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
+rm -rf yay 
 
 # Создания английских папок
 LC_ALL=C xdg-user-dirs-update --force
@@ -9,7 +12,12 @@ LC_ALL=C xdg-user-dirs-update --force
 rm -r Видео Документы Загрузки Изображения Музыка Общедоступные Рабочий\ стол/ Шаблоны
 
 # Установка i3
-yay -S --noconfirm i3-gaps rofi xorg xorg-xinit xorg-xrandr dunst i3status picom autotiling timeshift timeshift-autosnap lxappearance clipit firefox polkit-gnome man-pages-ru kotatogram-desktop-bin bleachbit ttf-font-awesome ttf-opensans ttf-kochi-substitute otf-ipafont noto-fonts ttf-droid ttf-liberation ttf-dejavu ttf-ubuntu-font-family nerd-fonts-ubuntu-mono powerline powerline-fonts ly-git open-vm-tools xf86-video-vmware xf86-input-vmmouse xf86-video-vesa --noconfirm
+yay -S --noconfirm i3-gaps rofi xorg xorg-xinit xorg-xrandr dunst i3status picom autotiling nitrogen timeshift timeshift-autosnap lxappearance clipit firefox polkit-gnome man-pages-ru kotatogram-desktop-bin bleachbit ttf-font-awesome ttf-opensans ttf-kochi-substitute otf-ipafont ttf-droid ttf-liberation ttf-dejavu ttf-ubuntu-font-family nerd-fonts-ubuntu-mono powerline powerline-fonts ly-git --noconfirm
+
+#noto-fonts 
+
+# Для VmWare (Закоментируйте если не надо)
+yay -S --noconfirm open-vm-tools xf86-video-vmware xf86-input-vmmouse xf86-video-vesa --noconfirm
 
 # Русская раскладка 
 sudo --no-ask-password localectl set-x11-keymap --no-convert us,ru pc105 "" grp:alt_shift_toggle
@@ -18,6 +26,8 @@ sudo --no-ask-password localectl set-x11-keymap --no-convert us,ru pc105 "" grp:
 sudo --no-ask-password systemctl enable vmtoolsd
 sudo --no-ask-password systemctl start vmtoolsd
 sudo --no-ask-password systemctl enable ly.service
+
+
 
 
 #sudo pacman -Syu
