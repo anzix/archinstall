55030049af6c816eb50a5a9cac113ff47ff9b970
Михаил <givefly3@gmail.com>
2021-08-29 14:17:21 +0000

    Update nextinstall.sh

--- nextinstall.sh
+++ nextinstall.sh
@@ -1,22 +1,23 @@
-#Установка Yay
+# Установка Yay
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 
-#Создания английских папок
+# Создания английских папок
 LC_ALL=C xdg-user-dirs-update --force
 #Удаляем рус папки
 rm -r Видео Документы Загрузки Изображения Музыка Общедоступные Рабочий\ стол/ Шаблоны
 
-#Установка i3
+# Установка i3
 yay -S --noconfirm i3-gaps rofi xorg xorg-xinit xorg-xrandr dunst i3status picom autotiling timeshift timeshift-autosnap lxappearance clipit firefox polkit-gnome man-pages-ru kotatogram-desktop-bin bleachbit ttf-font-awesome ttf-opensans ttf-kochi-substitute otf-ipafont noto-fonts ttf-droid ttf-liberation ttf-dejavu ttf-ubuntu-font-family nerd-fonts-ubuntu-mono powerline powerline-fonts ly-git open-vm-tools xf86-video-vmware xf86-input-vmmouse xf86-video-vesa --noconfirm
 
-#Русская раскладка 
-localectl set-x11-keymap --no-convert us,ru pc105 "" grp:alt_shift_toggle
+# Русская раскладка 
+sudo --no-ask-password localectl set-x11-keymap --no-convert us,ru pc105 "" grp:alt_shift_toggle
 
-systemctl enable vmtoolsd
-systemctl start vmtoolsd
-systemctl enable ly.service
+# Включение дм и другие штуки
+sudo --no-ask-password systemctl enable vmtoolsd
+sudo --no-ask-password systemctl start vmtoolsd
+sudo --no-ask-password systemctl enable ly.service
 
 
 #sudo pacman -Syu
