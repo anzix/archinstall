bda2962fd3eab89576e632d7f0941b8fcbf41932
anzix <givefly3@gmail.com>
2024-09-01 12:11:34 +0500

    Мелкое обновление vm_support.sh

--- vm_support.sh
+++ vm_support.sh
@@ -25,7 +25,11 @@ done
 # FIXME: исправить второй вариант Bridge
 # так как это НЕ мост по своей сути это
 # тот же NAT
+# https://www.redhat.com/sysadmin/setup-network-bridge-VM
+# https://wiki.archlinux.org/title/Network_bridge
 # https://linuxconfig.org/how-to-use-bridged-networking-with-libvirt-and-kvm
+# https://www.spad.uk/posts/really-simple-network-bridging-with-qemu/
+# https://www.altlinux.org/%D0%9D%D0%B0%D1%81%D1%82%D1%80%D0%BE%D0%B9%D0%BA%D0%B0_%D1%81%D0%B5%D1%82%D0%B8_%D0%B2_KVM#%D0%92%D0%BD%D1%83%D1%82%D1%80%D0%B5%D0%BD%D0%BD%D0%B8%D0%B9_%D0%B2%D0%B8%D1%80%D1%82%D1%83%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B9_%D0%BC%D0%BE%D1%81%D1%82
 
 # Установка сети VM
 if [ ${NETWORK_VM} = 'Default' ]; then
