557a74a4d815db1c98b8336ed0d0f3b23e1f0375
anzix <givefly3@gmail.com>
2025-01-19 20:20:15 +0500

    Добавил флаг --removable для grub
    Для того чтобы Grub не слетал если вынимать данный диск из системы

--- 1-chroot.sh
+++ 1-chroot.sh
@@ -365,9 +365,10 @@ chmod 700 /archinstall
 chown 1000:users -R /archinstall
 
 # Установка и настройка Grub
+# Флаг --removable необходим если вы собираетесь вынимать данный диск из системы
 #sed -i -e 's/#GRUB_DISABLE_OS_PROBER/GRUB_DISABLE_OS_PROBER/' /etc/default/grub # Обнаруживать другие ОС и добавлять их в grub (нужен пакет os-prober)
 # TODO: Для стандартизации (если точка монтирования /mnt/boot) выставить --efi-directory=/boot
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --removable
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # Врубаю сервисы
