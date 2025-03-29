5456d56326ab83a5a847569721d366ef56ec4f6f
anzix <givefly3@gmail.com>
2025-03-29 14:44:59 +0500

    Обновил gaming скрипт
    Конфиги для gaming вытягиваются из моего dotfiles

--- gaming.sh
+++ gaming.sh
@@ -6,19 +6,13 @@ sudo pacman -S --needed --noconfirm $(expac -S '%o' wine-staging) wine-staging w
 echo "==> Установка основных пакетов Pacman"
 yay -S --noconfirm --needed $(sed -e '/^#/d' -e 's/#.*//' -e "s/'//g" -e '/^\s*$/d' -e 's/ /\n/g' packages/gaming | column -t)
 
-# Экспорт игровых конфигов
-# cd ~/.dotfiles/base
-# stow -vt ~ mangohud vkBasalt
+# Вытягиваю игровые конфиги и файлы
+pushd ~/.dotfiles/gaming
+stow -vt ~ */
+popd
 
-# Автозапуск и тихий запуск Steam
-# INFO: при первом запуске steam сразу добавляется в autostart
-mkdir -pv ~/.config/autostart/
-ln -svi /usr/share/applications/steam.desktop ~/.config/autostart/
-sed -i 's|^Exec=/usr/bin/steam-runtime %U|Exec=/usr/bin/steam-runtime -silent %U|g' ~/.config/autostart/steam.desktop
-
-# Автозапуск Discord
-ln -svi /usr/share/applications/discord.desktop ~/.config/autostart/
-sed -i 's|^Exec=/usr/bin/discord|Exec=/usr/bin/discord --start-minimized|g' ~/.config/autostart/discord.desktop
+# INFO: иконки которые были вытянуты из dotfiles можно добавить в autostart
+# сунув их в ~/.config/autostart/. Поддерживает GNOME и KDE Plasma
 
 # Дать возможность gamemode выставлять приоритет процесса игры (renice)
 # https://wiki.archlinux.org/title/Gamemode#Renicing
